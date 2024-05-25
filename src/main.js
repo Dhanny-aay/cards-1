48a7b5a41f97a448dcd5be8df82f6a894a03d6fb
Dhanny-aay <danielakintolu@gmail.com>
2024-05-25 17:07:52 +0100

    "Updated index.html, main.js, output.css, and style.css with various changes to HTML structure, JavaScript functionality, and CSS styles."

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ const nextSlide = () => {
   let nextSlide = activeSlide.nextElementSibling;
   if (nextSlide) {
     activeSlide.classList.remove("slide--active");
-    nextSlide.classList.remove("next");
     nextSlide.classList.add("slide--active");
     renderSlides();
     renderBtns();
@@ -19,7 +18,6 @@ const prevSlide = () => {
   let prevSlide = activeSlide.previousElementSibling;
   if (prevSlide) {
     activeSlide.classList.remove("slide--active");
-    prevSlide.classList.remove("prev");
     prevSlide.classList.add("slide--active");
     renderSlides();
     renderBtns();
@@ -39,8 +37,19 @@ const updateButtonState = (prevBtn, nextBtn) => {
     : nextBtn.classList.add("disabled");
 };
 
+const updateTestimonialButtons = () => {
+  let activeSlide = document.querySelector(".slide--active");
+  let activeSlideIndex = activeSlide.getAttribute("data-slide");
+
+  document.querySelectorAll(".testimonial-button").forEach((button) => {
+    button.classList.remove("active-testimonial-button");
+    if (button.getAttribute("data-slide") === activeSlideIndex) {
+      button.classList.add("active-testimonial-button");
+    }
+  });
+};
+
 const renderBtns = () => {
-  // Update button states for both desktop and mobile buttons
   let nextBtn = document.querySelector("#forvard");
   let prevBtn = document.querySelector("#back");
   let nextBtnMob = document.querySelector("#forvardMob");
@@ -48,18 +57,19 @@ const renderBtns = () => {
 
   updateButtonState(prevBtn, nextBtn);
   updateButtonState(prevBtnMob, nextBtnMob);
+  updateTestimonialButtons();
 };
 
 const renderSlides = () => {
   let slides = document.querySelectorAll(".slide");
-  if (!slides) {
-    return;
-  }
+  if (!slides) return;
+
   let activeSlide = document.querySelector(".slide--active");
   if (!activeSlide) {
     activeSlide = slides.item(0);
     activeSlide.classList.add("slide--active");
   }
+
   slides.forEach((slide) => {
     slide.classList.remove("prev", "next");
   });
@@ -74,14 +84,10 @@ const renderSlides = () => {
 const renderSlider = (element) => {
   const slider = document.querySelector(element);
   if (slider) {
-    // Add event listeners for desktop buttons
     document.querySelector("#forvard").addEventListener("click", nextSlide);
     document.querySelector("#back").addEventListener("click", prevSlide);
-
-    // Add event listeners for mobile buttons
     document.querySelector("#forvardMob").addEventListener("click", nextSlide);
     document.querySelector("#backMob").addEventListener("click", prevSlide);
-
     renderSlides();
     renderBtns();
   }
